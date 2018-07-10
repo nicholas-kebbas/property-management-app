1d3e12f59151a9bbed19ab514f6a477d74a84b9e
dknoma <dknoma@dons.usfca.edu>
2018-07-09 22:03:32 -0700

    changed all datatypes to string

--- server/controllers/properties.js
+++ server/controllers/properties.js
@@ -28,47 +28,47 @@ module.exports = {
     return PropertyItem
     .findAll({
       where: {
-    [Op.or]: [
-      {allows_pets: req.body.allows_pets},
-      {zip: req.body.zip},
-      {state: req.body.state},
-      {city: req.body.city},
-      {number_of_bedrooms: req.body.number_of_bedrooms},
-      {number_of_bathrooms: req.body.number_of_bathrooms},
-      {prices: { [Op.gt]: req.body.prices } }
-    ]
-  }
-  })
-  .then(property_item => {
-  if (!property_item) {
-    return res.status(404).send({
-      message: 'property Not Found',
-    });
-  }
-  return res.status(200).send(property_item);
-  })
-  .catch(error => res.status(400).send(error));
+        [Op.or]: [
+          {allows_pets: req.body.allows_pets},
+          {zip: req.body.zip},
+          {state: req.body.state},
+          {city: req.body.city},
+          {number_of_bedrooms: req.body.number_of_bedrooms},
+          {number_of_bathrooms: req.body.number_of_bathrooms},
+          {prices: { [Op.gt]: req.body.prices } }
+        ]
+      }
+    })
+    .then(property_item => {
+    if (!property_item) {
+      return res.status(404).send({
+        message: 'property Not Found',
+      });
+    }
+    return res.status(200).send(property_item);
+    })
+    .catch(error => res.status(400).send(error));
   },
 
 
   list(req, res) {
-  return PropertyItem
-    .all()
-    .then(propertyItem => res.status(200).send(propertyItem))
-    .catch(error => res.status(400).send(error));
-},
+    return PropertyItem
+      .all()
+      .then(propertyItem => res.status(200).send(propertyItem))
+      .catch(error => res.status(400).send(error));
+  },
 
   retrieve(req, res) {
-  return PropertyItem
-    .findById(req.params.propertyId)
-    .then(property_item => {
-      if (!property_item) {
-        return res.status(404).send({
-          message: 'property Not Found',
-        });
-      }
-      return res.status(200).send(property_item);
-    })
-    .catch(error => res.status(400).send(error));
-  }
+    return PropertyItem
+      .findById(req.params.propertyId)
+      .then(property_item => {
+        if (!property_item) {
+          return res.status(404).send({
+            message: 'property Not Found',
+          });
+        }
+        return res.status(200).send(property_item);
+      })
+      .catch(error => res.status(400).send(error));
+    }
   };

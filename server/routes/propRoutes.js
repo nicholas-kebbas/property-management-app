1b6472ac56df7750ea2c1cec4adba347303f9baa
huan0750 <31259665+huan0750@users.noreply.github.com>
2018-07-09 17:07:34 -0700

    added filter and list functions

--- server/routes/propRoutes.js
+++ server/routes/propRoutes.js
@@ -1,16 +1,19 @@
-const propertyiesontroller = require('../controllers').properties;
+const propertiesController = require('../controllers').properties;
 
 module.exports = (app) => {
 	app.get('/api', (req, res) => res.status(200).send({
-		message: 'Welcome to the Todos API!',
+		message: 'Welcome to the property API!',
 	}));
 
-	/***********************************
-	 * Authentication related requests *
-	 ***********************************/
-    app.post('/api/property', propertyiesontroller.create);
-    
-    
+	/* create property */
+    app.post('/api/property/create', propertiesController.create);
+
+		/* list and filter property */
+		app.get('/api/property/list', propertiesController.list);
+		app.post('/api/property/filter',propertiesController.filter);
+		app.get('/api/property/:propertyId', propertiesController.retrieve);
+
+
 	app.all('/api/users', (req, res) =>
 		res.status(405).send({
 			message: 'Method Not Allowed',

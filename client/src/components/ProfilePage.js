96822a34c7c8c103520dc95a17335af2c200268e
Soo Jung Kim <soojungkim@Soos-MacBook-Pro.local>
2018-07-10 14:45:22 -0700

    updated links to buttons

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -20,9 +20,9 @@ class ProfilePage extends React.Component {
     if (this.props.user_type ==="propertymanager") {
       return (
         <div>
-          <div> <a href="/propertylisting"> View Property Listing</a></div>
+          <div> <a class="button" href="/propertylisting"> View Property Listing</a></div>
           <br/>
-          <div> <a href="/createproperty"> Create New Property</a></div>
+          <div> <a class="button" href="/createproperty"> Create New Property</a></div>
         </div>
       )
     }
